bbd1a73ce24e4f9fa472c5b7a7505472ee3b999a
woshinivek <woshinivek@yahoo.com>
2022-03-29 17:02:17 +0300

    changes

--- js/02-delegation.js
+++ js/02-delegation.js
@@ -1,3 +1,17 @@
+const containerRef = document.querySelector(".js-btns-collection");
 
+containerRef.addEventListener("click", onBtnClick);
 
+// function onBtnClick(evt) {
+//   console.dir(evt.target);
+//   console.log(evt.target.nodeName);
+// }
 
+function onBtnClick(evt) {
+  if (evt.target.nodeName !== "BUTTON") {
+    console.log("NOT IN BUTTON");
+    return;
+  }
+
+  console.log(`Click in the "${evt.target.textContent}"`);
+}
