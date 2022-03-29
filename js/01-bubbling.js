d220dab4e34e124fa6c7c3c8fa9fe83394879797
woshinivek <woshinivek@yahoo.com>
2022-03-29 14:33:42 +0300

    changes

--- js/01-bubbling.js
+++ js/01-bubbling.js
@@ -5,8 +5,13 @@ const refs = {
 };
 
 refs.parent.addEventListener("click", onParentClick);
-// refs.child.addEventListener("click", onChildClick);
+refs.child.addEventListener("click", onChildClick);
 refs.innerChild.addEventListener("click", onInnerChild);
+
+// window.addEventListener("click", (e) => {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
 
 function onParentClick(evt) {
   console.log("on parent click");
@@ -16,14 +21,26 @@ function onParentClick(evt) {
   //   evt.currentTarget.style.backgroundColor = "red";
 }
 
-// function onChildClick(evt) {
-//   console.log("on child click");
-//   console.log("on child click -> evt.target: ", evt.target);
-//   console.log("on child click -> evt.currentTarget: ", evt.currentTarget);
-// }
+function onChildClick(evt) {
+  console.log("on child click");
+  console.log("on child click -> evt.target: ", evt.target);
+  console.log("on child click -> evt.currentTarget: ", evt.currentTarget);
+}
 
 function onInnerChild(evt) {
   console.log("on inner-child click");
   console.log("on inner-child click -> evt.target: ", evt.target);
   console.log("on inner-child click -> evt.currentTarget: ", evt.currentTarget);
 }
+
+// const inputMeyblRef = document.querySelector('input[data-input="meybl"]');
+// console.log(inputMeyblRef);
+// console.log(inputMeyblRef.dataset.input);
+
+// const inputTupangRef = document.querySelector('input[data-input="tupang"]');
+// console.log(inputTupangRef);
+// console.log(inputTupangRef.dataset.input);
+
+// console.log(refs.parent);
+
+// console.log(inputRef.dataset);
