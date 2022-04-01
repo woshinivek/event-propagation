5f3753faf4c056de9fdb7b9c225e5e821fb85d2f
woshinivek <woshinivek@yahoo.com>
2022-04-01 16:21:33 +0300

    changes

--- js/03-workshop-tags.js
+++ js/03-workshop-tags.js
@@ -1,29 +1,55 @@
+// const tagsContainerRef = document.querySelector(".js-tags");
+// let selectedTag = null;
+
+// tagsContainerRef.addEventListener("click", onTagsContainerClick);
+
+// function onTagsContainerClick(e) {
+//   if (e.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+
+//   const currentActiveBtn = document.querySelector(".tags__btn-active");
+//   console.log(currentActiveBtn);
+
+//   if (currentActiveBtn) {
+//     currentActiveBtn.classList.remove("tags__btn-active");
+//   }
+
+//   const nextActiveBtn = e.target;
+//   nextActiveBtn.classList.add("tags__btn-active");
+//   selectedTag = nextActiveBtn.dataset.value;
+
+//   if (currentActiveBtn === nextActiveBtn) {
+//     currentActiveBtn.classList.remove("tags__btn-active");
+//   }
+
+//   // console.dir(currentActiveBtn?.nodeName);
+// }
+
+//--------------------------------------------------------------------
+//--------------------------------------------------------------------
+
 const tagsContainerRef = document.querySelector(".js-tags");
 let selectedTag = null;
 
 tagsContainerRef.addEventListener("click", onTagsContainerClick);
 
-function onTagsContainerClick(e) {
-  if (e.target.nodeName !== "BUTTON") {
+function onTagsContainerClick(evt) {
+  if (evt.target.nodeName !== "BUTTON") {
     return;
   }
 
   const currentActiveBtn = document.querySelector(".tags__btn-active");
-  console.log(currentActiveBtn);
 
   if (currentActiveBtn) {
     currentActiveBtn.classList.remove("tags__btn-active");
   }
 
-  const nextActiveBtn = e.target;
+  const nextActiveBtn = evt.target;
   nextActiveBtn.classList.add("tags__btn-active");
   selectedTag = nextActiveBtn.dataset.value;
 
-  if (currentActiveBtn === nextActiveBtn) {
-    currentActiveBtn.classList.remove("tags__btn-active");
-  }
-
-  // console.dir(currentActiveBtn?.nodeName);
+  console.log(selectedTag);
 }
 
 //--------------------------------------------------------------------
