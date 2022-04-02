208254d142b73f0934ce44d207907441b179936e
woshinivek <woshinivek@yahoo.com>
2022-04-02 15:43:41 +0300

    changes

--- js/04-workshop-colorpicker.js
+++ js/04-workshop-colorpicker.js
@@ -5,23 +5,31 @@ const markup = createColorCardsMarkup(colors);
 
 paletteContainer.insertAdjacentHTML("beforeend", markup);
 
+paletteContainer.addEventListener("click", onColorPaletteClick);
+
 function createColorCardsMarkup(colors) {
   return colors
     .map(({ hex, rgb }) => {
       return `
         <div class="color-card">
         <div
-          class="color-swatch"
-          data-hex="${hex}"
-          data-rgb="${rgb}"
-          style="background-color: ${hex}"
+        class="color-swatch"
+        data-hex="${hex}"
+        data-rgb="${rgb}"
+        style="background-color: ${hex}"
         ></div>
         <div class="color-meta">
-          <p>HEX: ${hex}</p>
-          <p>RGB: ${rgb}</p>
+        <p>HEX: ${hex}</p>
+        <p>RGB: ${rgb}</p>
         </div>
-      </div>
-      `;
+        </div>
+        `;
     })
     .join("");
+}
+
+function onColorPaletteClick(evt) {
+  if (evt.target.classList.contains("color-swatch")) {
+    console.log(evt.target);
+  }
 }
