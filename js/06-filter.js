672bc58eb72be2685949bceb609eb6498e395073
woshinivek <woshinivek@yahoo.com>
2022-04-18 11:39:44 +0300

    changes

--- js/06-filter.js
+++ js/06-filter.js
@@ -5,8 +5,18 @@ const refs = {
   input: document.querySelector("#filter"),
 };
 
+refs.input.addEventListener("input", onFilterChange);
+
 const techsMarkup = createElementsMarkup(techs);
 refs.list.innerHTML = techsMarkup;
+
+function onFilterChange(evt) {
+  const filter = evt.target.value.toLowerCase();
+
+  const filteredItems = techs.filter((tech) =>
+    tech.label.toLowerCase().includes(filter)
+  );
+}
 
 function createElementsMarkup(items) {
   return items.map((item) => `<li>${item.label}</li>`).join("");
