1f2d4bfbfc4ad273aa9df5f03bd14741660c1d73
woshinivek <woshinivek@yahoo.com>
2022-04-19 22:01:42 +0300

    changes

--- js/08-lazysizes.js
+++ js/08-lazysizes.js
@@ -1,1 +1,38 @@
+if ("loading" in HTMLImageElement.prototype) {
+  console.log();
+  const images = document.querySelectorAll('img[loading="lazy"]');
 
+  images.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+} else {
+  const script = document.createElement("script");
+
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+
+  //   script.setAttribute(
+  //     "integrity",
+  //     "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
+  //   );
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+
+  //   script.setAttribute("crossorigin", "anonymous");
+  script.crossOrigin = "anonymous";
+
+  //   script.setAttribute("referrerpolicy", "no-referrer");
+  script.referrerPolicy = "no-referrer";
+
+  document.body.appendChild(script);
+}
+
+const images = document.querySelectorAll("img[data-src]");
+
+images.forEach((img) =>
+  img.addEventListener("load", onImageLoaded, { once: true })
+);
+
+function onImageLoaded(evt) {
+  evt.target.classList.add("appear");
+}
