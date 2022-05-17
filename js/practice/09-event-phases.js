e3e8fc680e3609131c287b6a1393ea8d1c38468c
woshinivek <woshinivek@yahoo.com>
2022-05-17 17:15:35 -0400

    learned how to event phase works

--- js/practice/09-event-phases.js
+++ js/practice/09-event-phases.js
@@ -8,7 +8,30 @@ const EVENT_PHASES = {
 /*
 1. Протестировать основные принципы распространения
 (event propagation) и всплытия (event bubbling)событий
-
-
-
 */
+
+const itemDivs = document.querySelectorAll(".itemDiv");
+
+window.onload = () => {
+  itemDivs.forEach((item) => item.addEventListener("click", onItemClick));
+};
+
+function onItemClick(evt) {
+  evt.stopPropagation();
+  console.log(evt.eventPhase);
+  if (evt.eventPhase === EVENT_PHASES.TARGET) {
+    clearStyles();
+    evt.target.classList.add("selected");
+  }
+}
+
+function clearStyles() {
+  itemDivs.forEach((item) => {
+    if (item.classList.contains("selected")) {
+      item.classList.remove("selected");
+    }
+  });
+}
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
