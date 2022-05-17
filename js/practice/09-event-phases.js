6e8322991c67dabec21cee813cb408ea5eabd921
woshinivek <woshinivek@yahoo.com>
2022-05-17 12:36:18 -0400

    task 1 added

---
+++ js/practice/09-event-phases.js
@@ -0,0 +1,14 @@
+const EVENT_PHASES = {
+  NONE: 0,
+  CAPTURE: 1,
+  TARGET: 2,
+  BUBBLE: 3,
+};
+
+/*
+1. Протестировать основные принципы распространения
+(event propagation) и всплытия (event bubbling)событий
+
+
+
+*/
