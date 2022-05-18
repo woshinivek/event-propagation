c1e42e22686d9897a411eaeae8e813d496aa9cb4
woshinivek <woshinivek@yahoo.com>
2022-05-18 15:48:03 -0400

    added second method of change square bcg color

--- js/practice/10-square.js
+++ js/practice/10-square.js
@@ -1,3 +1,5 @@
+import colorsForPractice from "./colors-practice.js";
+
 /* 
   2. Используя делегирование событий обработать 
   нажатие любой кнопки внутри #container
@@ -7,3 +9,84 @@
   и вернуть квадрат в исходный вид
   см. в index.html
 */
+
+// const btnContainer = document.getElementById("container");
+// const squareRef = document.getElementById("square");
+
+// window.onload = () => {
+//   btnContainer.addEventListener("click", onBtnClick);
+// };
+
+// function onBtnClick(evt) {
+//   if (evt.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+
+//   squareRef.style.backgroundColor = color;
+
+//   const btn = evt.target;
+
+//   if (btn.classList.contains("activeBtn")) {
+//     btn.classList.remove("activeBtn");
+//   }
+
+//   removeActiveBtn();
+//   addActiveBtnClass(btn);
+//   setSquareColor(btn.dataset.color);
+// }
+
+// function removeActiveBtn() {
+//   const activeBtn = document.querySelector(".activeBtn");
+
+//   if (activeBtn) {
+//     activeBtn.classList.remove("activeBtn");
+//   }
+// }
+
+// function addActiveBtnClass(btn) {
+//   btn.classList.add("activeBtn");
+// }
+
+// function setSquareColor(color) {
+//   squareRef.style.backgroundColor = color;
+// }
+
+// ----------------------------------------------------------
+
+const btnContainer = document.getElementById("container");
+const squareRef = document.getElementById("square");
+let prevStyle = null;
+
+window.onload = () => {
+  btnContainer.addEventListener("click", setColorSquare);
+};
+
+function setColorSquare(evt) {
+  const { target } = evt;
+
+  if (evt.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  squareRef.style.backgroundColor = target.dataset.color;
+  setActiveBtn(target);
+}
+
+function setActiveBtn(target) {
+  const activeBtn = document.querySelector("button.activeBtn");
+
+  if (prevStyle === target.dataset.color) {
+    activeBtn.classList.remove("activeBtn");
+    squareRef.style.backgroundColor = "";
+    prevStyle = null;
+  } else {
+    prevStyle = target.dataset.color;
+
+    if (activeBtn) {
+      activeBtn.classList.remove("activeBtn");
+    }
+    target.classList.add("activeBtn");
+  }
+}
+
+// ----------------------------------------------------------
---
+++ js/practice/colors-practice.js
@@ -0,0 +1,7 @@
+export default [
+  { hex: "#2f2fe3" },
+  { hex: "#2ec72e" },
+  { hex: "#f25656" },
+  { hex: "#212121" },
+  { hex: "#ffc107" },
+];
