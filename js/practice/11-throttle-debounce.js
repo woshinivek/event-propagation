7085215e3760413a96960b8ab21338b842ab07c2
woshinivek <woshinivek@yahoo.com>
2022-05-18 23:33:53 -0400

    reviewed with throttle & debounce

--- js/practice/11-throttle-debounce.js
+++ js/practice/11-throttle-debounce.js
@@ -1,3 +1,19 @@
-const container = document.getElementById("container");
+// window.addEventListener("scroll", _.throttle(onWindowScroll, 400));
+// window.addEventListener("input", _.throttle(onInputChange, 400));
+window.addEventListener("input", _.debounce(onInputChange, 400));
 
-console.log(container);
+const statContainerRef = document.getElementById("statContainer");
+const outputRef = document.getElementById("output");
+
+let counter = 0;
+
+// function onWindowScroll(evt) {
+//   statContainerRef.innerHTML = counter += 1;
+// }
+
+function onInputChange(e) {
+  statContainerRef.innerHTML = counter += 1;
+  outputRef.textContent = `Output: ${e.target.value}`;
+
+  console.log(e.target.value);
+}
