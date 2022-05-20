b973f62d435fab85c835d5f56ba44f4ffa6072c2
woshinivek <woshinivek@yahoo.com>
2022-05-20 14:00:07 -0400

    got full understanding of intersection observer, basic things

--- js/practice/12-IntersectionObserver.js
+++ js/practice/12-IntersectionObserver.js
@@ -3,26 +3,32 @@ const el6Ref = document.getElementById("el6");
 const options = {
   //   rootMargin: "-30px -30px",
   rootMargin: "0px",
-  threshold: 1,
+  threshold: 0.5,
 };
 
-// const onEntry = function (entries, observer) {
-//   console.log("entries: ", entries);
-//   console.log("observer: ", observer);
+const onEntry = function (entries, observer) {
+  console.log("entries: ", entries);
+  // console.log("observer: ", observer);
 
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("checked");
+      entry.target.style.backgroundColor = "red";
+    }
+  });
+};
+
+// const testy = function (entries, observer) {
+//   console.log("Threshold reached!");
 //   el6Ref.style.backgroundColor = "red";
+//   //debugger;
 // };
-
-const testy = function (entries, observer) {
-  console.log("Threshold reached!");
-  el6Ref.style.backgroundColor = "red";
-  //debugger;
-};
-//const observer = new IntersectionObserver(_.debounce(onEntry, 1000), options);
-const observer = new IntersectionObserver(_.debounce(testy, 500), options);
+// const observer = new IntersectionObserver(_.debounce(onEntry, 1000), options);
+const observer = new IntersectionObserver(onEntry, options);
+// const observer = new IntersectionObserver(_.debounce(testy, 500), options);
 
 // window.addEventListener('load', (event) => {
-//   observer.observe(el6Ref);
+observer.observe(el6Ref);
 // });
 
 console.log(observer);
