724c1aee9e5d904eadfbfdff1ae0a15de7b8f19b
woshinivek <woshinivek@yahoo.com>
2022-05-21 15:59:51 -0400

    repeated intersection observer

--- js/practice/12-IntersectionObserver.js
+++ js/practice/12-IntersectionObserver.js
@@ -1,36 +1,96 @@
-const el6Ref = document.getElementById("el6");
+import colors from "./colors-practice.js";
 
-const options = {
-  //   rootMargin: "-30px -30px",
-  rootMargin: "0px",
-  threshold: 0.5,
-};
+// const el6Ref = document.getElementById("el6");
 
-const onEntry = function (entries, observer) {
-  console.log("entries: ", entries);
-  // console.log("observer: ", observer);
-
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("checked");
-      entry.target.style.backgroundColor = "red";
-    }
-  });
-};
-
-// const testy = function (entries, observer) {
-//   console.log("Threshold reached!");
-//   el6Ref.style.backgroundColor = "red";
-//   //debugger;
+// const options = {
+//   //   rootMargin: "-30px -30px",
+//   rootMargin: "0px",
+//   threshold: 0.5,
 // };
-// const observer = new IntersectionObserver(_.debounce(onEntry, 1000), options);
-const observer = new IntersectionObserver(onEntry, options);
-// const observer = new IntersectionObserver(_.debounce(testy, 500), options);
 
-// window.addEventListener('load', (event) => {
-observer.observe(el6Ref);
+// const onEntry = function (entries, observer) {
+//   console.log("entries: ", entries);
+//   // console.log("observer: ", observer);
+
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("checked");
+//       entry.target.style.backgroundColor = "red";
+
+//       observer.unobserve(entry.target);
+//     }
+//   });
+// };
+
+// // const testy = function (entries, observer) {
+// //   console.log("Threshold reached!");
+// //   el6Ref.style.backgroundColor = "red";
+// //   //debugger;
+// // };
+// // const observer = new IntersectionObserver(_.debounce(onEntry, 1000), options);
+// const observer = new IntersectionObserver(onEntry, options);
+// // const observer = new IntersectionObserver(_.debounce(testy, 500), options);
+
+// // window.addEventListener('load', (event) => {
+// observer.observe(el6Ref);
+// // });
+
+// console.log(observer);
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+
+// const targetElems = document.querySelectorAll(".addScroll");
+
+// const options = {
+//   // rootMargin: "20px",
+//   threshold: [0.2, 0.4, 0.6, 0.8, 1],
+//   // threshold: 0.5,
+// };
+
+// const onEntry = (entries, observer) =>
+//   entries.forEach((entry) => {
+//     // let count = 0;
+//     if (entry.isIntersecting) {
+//       // entry.target.classList.add("checked");
+//       console.log(entry.intersectionRatio);
+//       entry.target.style.backgroundColor = "red";
+//       entry.target.style.opacity = entry.intersectionRatio;
+
+//       // observer.unobserve(entry.target);
+//       // count++;
+//     }
+//   });
+// const observer = new IntersectionObserver(onEntry, options);
+
+// targetElems.forEach((elem) => {
+//   observer.observe(elem);
 // });
 
-console.log(observer);
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+
+// const el6Ref = document.getElementById("el6");
+// const elements = document.querySelectorAll(".addScroll");
+
+// const options = {
+//   threshold: [0.2, 0.4, 0.6, 0.8, 1],
+// };
+
+// function onEntry(entries, observer) {
+//   console.log(observer);
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       console.log(entry.intersectionRatio);
+//       entry.target.style.backgroundColor = "red";
+//       entry.target.style.opacity = entry.intersectionRatio;
+//     }
+//   });
+// }
+
+// const observer = new IntersectionObserver(onEntry, options);
+// elements.forEach((element) => {
+//   observer.observe(element);
+// });
+
 // ---------------------------------------------------------------
 // ---------------------------------------------------------------
